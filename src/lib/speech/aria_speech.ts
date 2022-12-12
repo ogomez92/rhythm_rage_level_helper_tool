5bd13057f7ee91349046ca1f1d11d9cfe2a0879c
ogomez92 <ogomez.s92@gmail.com>
2022-12-12 19:06:22 +0100

    feat: aDd Aria tts

---
+++ src/lib/speech/aria_speech.ts
@@ -0,0 +1,25 @@
+class AriaSpeechEngine {
+    private HTMLElement: HTMLDivElement;
+    private lastSpokenString: string;
+
+    constructor() {
+        const screenReaderElement = document.createElement('div');
+        screenReaderElement.setAttribute('aria-live', 'assertive');
+        screenReaderElement.setAttribute('aria-atomic', 'true');
+        screenReaderElement.setAttribute('aria-relevant', 'text');
+        screenReaderElement.setAttribute('role', 'status');
+        screenReaderElement.setAttribute('style', 'position: absolute; top: -100px; left: -100px; width: 1px; height: 1px; overflow: hidden;');
+        document.body.appendChild(screenReaderElement);
+        this.HTMLElement = screenReaderElement;
+    }
+
+    speak(text: string) {
+        if (text == this.lastSpokenString && text !== '') {
+            this.speak('');
+        }
+
+        this.HTMLElement.innerText = text;
+    }
+}
+
+export default AriaSpeechEngine;
