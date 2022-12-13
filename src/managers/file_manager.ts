f791101092ede607d6c990ff7fe3708a01dc8d66
ogomez92 <ogomez.s92@gmail.com>
2022-12-13 09:43:06 +0100

    fix: Fix broken file manager if path not found

--- src/managers/file_manager.ts
+++ src/managers/file_manager.ts
@@ -1,26 +1,32 @@
-import * as fs from 'fs';
-import FileInfo from '@src/domain/file_info'
+import * as fs from "fs";
+import FileInfo from "@src/domain/file_info";
 
 export default class fileManager {
-    private path: string;
+  private path: string;
 
-    constructor(path: string) {
-        this.path = path;
+  constructor(path: string) {
+    try {
+      this.path = path;
+    } catch {
+      this.path = "";
     }
+  }
 
-    public getPath = () => this.path;
+  public getPath = () => this.path;
 
-    public getFileList = (): FileInfo[] => {
-
-        const files = fs.readdirSync(this.path);
-        const fileList: FileInfo[] = [];
-        files.forEach((file) => {
-            const stats = fs.statSync(`${this.path}/${file}`);
-            const size = stats.size;
-            const fileInfo = new FileInfo(file, size);
-            fileList.push(fileInfo);
-        });
-
-        return fileList;
+  public getFileList = (): FileInfo[] => {
+    if (!this.path) {
+        return [];
     }
+    const files = fs.readdirSync(this.path);
+    const fileList: FileInfo[] = [];
+    files.forEach((file) => {
+      const stats = fs.statSync(`${this.path}/${file}`);
+      const size = stats.size;
+      const fileInfo = new FileInfo(file, size);
+      fileList.push(fileInfo);
+    });
+
+    return fileList;
+  };
 }
