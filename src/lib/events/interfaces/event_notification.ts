201daa23e8f37115552df337df21982855105470
oriol <ogomez.s92@gmail.com>
2022-12-25 08:12:12 +0100

    chore: Fix formatting

--- src/lib/events/interfaces/event_notification.ts
+++ src/lib/events/interfaces/event_notification.ts
@@ -2,9 +2,9 @@ import EventType from "../enums/event_type";
 import EventManager from "../event_manager";
 
 interface EventNotification {
-type: EventType;
-dispatcher: EventManager;
-data?: never;
+    type: EventType;
+    dispatcher: EventManager;
+    data?: never;
 }
 
 export default EventNotification;
