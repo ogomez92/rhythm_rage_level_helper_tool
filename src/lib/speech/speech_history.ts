bff0559bdff36db712d8a78b9efe85226326fb6e
oriol <ogomez.s92@gmail.com>
2022-12-25 08:11:53 +0100

    feat: add speech history library

---
+++ src/lib/speech/speech_history.ts
@@ -0,0 +1,99 @@
+import SpeechManager from "@lib/speech/speech_manager";
+import KeyboardInput from "@lib/input/keyboard_input";
+import EventType from "@lib/events/enums/event_type";
+import EventSubscriber from "@lib/events/interfaces/event_subscriber";
+import EventNotification from "@lib/events/interfaces/event_notification";
+import KeyboardKeycode from "@lib/input/enums/keyboard_keycode";
+
+export default class SpeechHistory implements EventSubscriber {
+    private buffer: string[];
+    private speaker: SpeechManager;
+    private input: KeyboardInput;
+    private position: number;
+    private active: boolean;
+
+    constructor(speechManager: SpeechManager) {
+        this.input = new KeyboardInput();
+        this.active = true;
+        this.input.subscribe(EventType.KEYBOARD_KEY_PRESSED, this)
+        this.speaker = speechManager;
+        this.buffer = [];
+        this.position = 0;
+    }
+
+    public onNotificationReceived = (event: EventNotification): void => {
+        const keyPressed: any = event.data;
+
+        if (keyPressed >= KeyboardKeycode.KEY0 && keyPressed <= KeyboardKeycode.KEY9) {
+            if (this.input.isPressed(KeyboardKeycode.ALT)) {
+                let positionToSpeak = keyPressed - KeyboardKeycode.KEY0;
+                if (positionToSpeak == 0) {
+                    positionToSpeak = 10;
+                }
+                
+                this.speakBufferMessageAtPositionFromBottom(positionToSpeak);
+            }
+        }
+        else if (keyPressed == KeyboardKeycode.PAGEDOWN) {
+            this.increasePosition();
+            this.speakBufferMessageAtPosition(this.position);
+        }
+        else if (keyPressed == KeyboardKeycode.PAGEUP) {
+            this.decreasePosition();
+            this.speakBufferMessageAtPosition(this.position);
+        }
+    }
+
+    public decreasePosition = () => {
+        if (this.input.isPressed(KeyboardKeycode.CTRL)) {
+            this.position = 0;
+            return;
+        }
+
+        if (this.position > 0) {
+            this.position--;
+        }
+    }
+
+    public increasePosition = () => {
+        if (this.input.isPressed(KeyboardKeycode.CTRL)) {
+            this.position = this.buffer.length - 1;
+            return;
+        }
+
+        if (this.position < this.buffer.length - 1) {
+            this.position++;
+        }
+    }
+
+    public speakBufferMessageAtPosition = (position = this.position) => {
+        this.stop();
+
+        if (position >= 0 && position < this.buffer.length) {
+            this.speaker.speak(this.buffer[position]);
+        }
+        else {
+            // Todo: Translate this
+            this.speaker.speak(`No message at position ${position}`);
+        }
+    }
+
+    public speakBufferMessageAtPositionFromBottom = (position = this.position) => {
+        this.stop();
+        if (position > 0 && position <= this.buffer.length) {
+            this.speaker.speak(this.buffer[this.buffer.length - position]);
+        }
+        else {
+            this.speaker.speak(`No message at position ${position}`);
+        }
+    }
+
+    public add = (text: string) => {
+        this.speaker.speak(text);
+        this.buffer.push(text);
+    }
+
+    public stop = () => this.speaker.stop();
+
+    public getPosition = (): number => this.position;
+}
---
+++ src/lib/speech/tests/speech_history.spec.ts
@@ -0,0 +1,149 @@
+import SpeechHistory from "@lib/speech/speech_history";
+import SpeechManager from "@lib/speech/speech_manager";
+import SpeechEngineType from "@lib/speech/enums/speech_engine_type";
+import KeyboardKeycode from "@lib/input/enums/keyboard_keycode";
+import KeyboardKey from "@lib/input/enums/keyboard_key";
+
+describe('speech history', () => {
+    let history: SpeechHistory;
+    beforeEach(async () => {
+        document.body.innerHTML = `        <div id="root"></div>        `;
+
+        const manager = new SpeechManager(SpeechEngineType.ARIA);
+
+        await manager.initialize();
+
+        history = new SpeechHistory(manager);
+    })
+
+    it('tries to access the first element without any elements added', () => {
+        const event = new KeyboardEvent("keydown", {
+            key: KeyboardKey.Alt,
+            which: KeyboardKeycode.ALT,
+        });
+        document.dispatchEvent(event);
+
+        const event2 = new KeyboardEvent("keydown", {
+            key: '1',
+            which: KeyboardKeycode.KEY1,
+        });
+        document.dispatchEvent(event2);
+
+        const ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+
+        expect(ariaSpeechText).toMatch(/No message at position 1/);
+    })
+
+    it('tries to access the 10th element without any elements added', () => {
+        const event = new KeyboardEvent("keydown", {
+            key: KeyboardKey.Alt,
+            which: KeyboardKeycode.ALT,
+        });
+        document.dispatchEvent(event);
+
+        const event2 = new KeyboardEvent("keydown", {
+            key: '0',
+            which: KeyboardKeycode.KEY0,
+        });
+        document.dispatchEvent(event2);
+
+        const ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+
+        expect(ariaSpeechText).toMatch(/No message at position 10/);
+    })
+
+    it('Adds some messages and moves among them', () => {
+        history.add('first');
+        history.add('second');
+        history.add('third');
+        history.add('fourth');
+
+        const event = new KeyboardEvent("keydown", {
+            key: KeyboardKey.Control,
+            which: KeyboardKeycode.CTRL,
+        });
+        document.dispatchEvent(event);
+
+        const event2 = new KeyboardEvent("keydown", {
+            key: KeyboardKey.PageUp,
+            which: KeyboardKeycode.PAGEUP,
+        });
+        document.dispatchEvent(event2);
+
+        let ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('first')
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: KeyboardKey.PageDown,
+            which: KeyboardKeycode.PAGEDOWN,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('fourth')
+
+        document.dispatchEvent(new KeyboardEvent("keyup", {
+            key: KeyboardKey.Control,
+            which: KeyboardKeycode.CTRL,
+        }));
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: KeyboardKey.PageUp,
+            which: KeyboardKeycode.PAGEUP,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('third')
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: KeyboardKey.PageUp,
+            which: KeyboardKeycode.PAGEUP,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('second')
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: KeyboardKey.PageUp,
+            which: KeyboardKeycode.PAGEUP,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('first')
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: KeyboardKey.PageUp,
+            which: KeyboardKeycode.PAGEUP,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('first')
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: KeyboardKey.Alt,
+            which: KeyboardKeycode.ALT,
+        }));
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: '1',
+            which: KeyboardKeycode.KEY1,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('fourth')
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: '2',
+            which: KeyboardKeycode.KEY2,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toBe('third')
+
+        document.dispatchEvent(new KeyboardEvent("keydown", {
+            key: '5',
+            which: KeyboardKeycode.KEY5,
+        }));
+
+        ariaSpeechText = document.getElementById("ariaSpeech").innerText;
+        expect(ariaSpeechText).toMatch(/No message at position 5/)
+
+    })
+})
