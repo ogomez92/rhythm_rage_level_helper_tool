0e99af3ee0d6ac099abca0a45ec5ab4018fe2aaf
oriol <ogomez.s92@gmail.com>
2022-12-26 12:54:33 +0100

    feat: aDd wait until

--- src/lib/helpers/time_helper.ts
+++ src/lib/helpers/time_helper.ts
@@ -1,4 +1,14 @@
 export default class TimeHelper {
   public static sleep = (milliseconds: number): Promise<void> =>
     new Promise((resolve) => setTimeout(resolve, milliseconds));
+
+  public static waitUntil = (condition: () => boolean): Promise<void> =>
+    new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (condition()) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 10);
+    });
 }
