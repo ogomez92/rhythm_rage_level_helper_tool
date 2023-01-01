530b2395a0db9672dc1fc925cd9ec99d0df91fcf
oriol <ogomez.s92@gmail.com>
2023-01-01 10:07:45 +0100

    docs: remove already done stuff from notes file and delete logfile

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import SoundManager from "@lib/sound/sound_manager";
 import Menu from "@lib/menu/menu";
 import TextItem from "./lib/menu/text_item";
 import SpeechEngineType from "./lib/speech/enums/speech_engine_type";
+import SoundItem from "./lib/menu/sound_item";
+import LocalizationService from "@lib/localization/localization_service";
+import Language from "./lib/localization/enums/language";
 
 window.onload = () => {
   setup();
@@ -27,21 +30,24 @@ async function setup() {
   const manager = new SpeechManager(SpeechEngineType.ARIA);
   await manager.initialize();
   const sm = new SoundManager();
-  sm.setExtension('ogg');
+  sm.setExtension('mp3');
   const items = [
-    new TextItem('first', 'hello', manager),
-    new TextItem('second', 'fuck', manager),
-    new TextItem('third', 'you', manager),
+    new TextItem('first', 'hello', manager, '1'),
+    new TextItem('second', 'fuck', manager, 'a'),
+    new TextItem('third', 'you', manager, 'b'),
+    new SoundItem('fourth', await sm.create('stest/menu1')),
+    new SoundItem('fifth', await sm.create('stest/menu2')),
   ]
+  sm.setExtension('ogg')
   const menu = new Menu(items, manager);
-menu.setIntroSound(await sm.create('stest/intro'))
-menu.setIntroText('hi');
-menu.setMoveSound(await sm.create('stest/move'));
-menu.setWrapSound(await sm.create('stest/wrap'))
-menu.setSelectSound(await sm.create('stest/select'))
-  let position = await menu.display();
-  console.log(position);
-  position = await menu.display();
-  console.log(position);
+  menu.setAllowFirstTimeNavigation(true);
+  menu.setIntroSound(await sm.create('stest/intro'))
+  menu.setIntroText('hi');
+  menu.setMoveSound(await sm.create('stest/move'));
+  menu.setWrapSound(await sm.create('stest/wrap'))
+  menu.setSelectSound(await sm.create('stest/select'))
+  menu.setEscapeOptionById('fifth');
+  await menu.showToUser();
+  await menu.showToUser();
   menu.destroy();
 }
