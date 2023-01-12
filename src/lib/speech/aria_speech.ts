1e4c6261dc5331195cfab35c5fd2082c00f9a39a
Oriol <ogomez.s92@gmail.com>
2023-01-12 08:06:16 +0100

    fix: fix aria tts on mac

--- src/lib/speech/aria_speech.ts
+++ src/lib/speech/aria_speech.ts
@@ -1,35 +1,37 @@
 import SpeechEngine from "@lib/speech/interfaces/speech_engine";
 
-class AriaSpeechEngine implements SpeechEngine{
-    private HTMLElement: HTMLDivElement;
-    private lastSpokenString: string;
+class AriaSpeechEngine implements SpeechEngine {
+  private HTMLElement: HTMLDivElement;
+  private lastSpokenString: string;
 
-    constructor() {
-        const screenReaderElement = document.createElement('div');
-        screenReaderElement.setAttribute('aria-live', 'polite');
-        screenReaderElement.setAttribute('id', 'ariaSpeech');
-        screenReaderElement.setAttribute('aria-atomic', 'true');
-        screenReaderElement.setAttribute('aria-relevant', 'text');
-        screenReaderElement.setAttribute('role', 'status');
-        screenReaderElement.setAttribute('style', 'position: absolute; top: -100px; left: -100px; width: 1px; height: 1px; overflow: hidden;');
-        document.body.appendChild(screenReaderElement);
-        this.HTMLElement = screenReaderElement;
+  constructor() {
+    const screenReaderElement = document.createElement("div");
+    screenReaderElement.setAttribute("aria-live", "polite");
+    screenReaderElement.setAttribute("id", "ariaSpeech");
+    screenReaderElement.setAttribute("aria-atomic", "true");
+    screenReaderElement.setAttribute("role", "status");
+    screenReaderElement.setAttribute(
+      "style",
+      "position: absolute; top: -100px; left: -100px; width: 1px; height: 1px; overflow: hidden;"
+    );
+    document.body.appendChild(screenReaderElement);
+    this.HTMLElement = screenReaderElement;
+  }
+
+  speak(text: string) {
+    if (text === this.lastSpokenString && text !== "") {
+      this.speak("");
     }
 
-    speak(text: string) {
-        if (text === this.lastSpokenString && text !== '') {
-            this.speak('');
-        }
+    this.HTMLElement.innerText = text;
+  }
 
-        this.HTMLElement.innerText = text;
-    }
+  public stop = () => (this.HTMLElement.innerText = "");
 
-    public stop = () => this.HTMLElement.innerText = ''
-
-    public destroy = () => {
-        this.HTMLElement.remove();
-        this.HTMLElement = null;
-    }
+  public destroy = () => {
+    this.HTMLElement.remove();
+    this.HTMLElement = null;
+  };
 }
 
 export default AriaSpeechEngine;
